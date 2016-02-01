456db95b7cd63f1a37585ce5842a4d8fdddeb72d
Nick Hamel <nicholas.g.hamel@maine.edu>
2016-02-01 09:42:02 -0500

    Finished development in Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		System.out.println("Here ye Here ye: important message");
 		System.out.println("Hello World");
 		System.out.println("That took way too much effort...");
+		System.out.println("See ya later!");
 	}
 
 }
