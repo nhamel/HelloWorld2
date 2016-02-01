3d7740702c37320bdca4613cff81b1fec1a60007
Nick Hamel <nicholas.g.hamel@maine.edu>
2016-02-01 09:38:42 -0500

    Started a new feature in Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		System.out.println("Here ye Here ye: important message");
 		System.out.println("Hello World");
 		System.out.println("That took way too much effort...");
 	}
