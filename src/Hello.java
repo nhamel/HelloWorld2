85e4a932508aab971bd564d5d15ad5aa9ad6487a
Nick Hamel <nicholas.g.hamel@maine.edu>
2016-02-07 17:27:31 -0500

    Reverting the changes for the failed merge conflict attempt...

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,6 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Here ye Here ye: important message");
-		System.out.println("HERE COMES SOME MERGE CONFLICTS!");
 		System.out.println("Hello World");
 		System.out.println("That took way too much effort...");
 		System.out.println("See ya later!");
