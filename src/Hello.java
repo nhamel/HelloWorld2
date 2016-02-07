ea866e5caf7610b8dcf3392794b1189452f7a712
Nick Hamel <nicholas.g.hamel@maine.edu>
2016-02-07 18:34:25 -0500

    Reverting again.  This is the last try before I just give up...

--- src/Hello.java
+++ src/Hello.java
@@ -4,9 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Here ye Here ye: important message");
 		System.out.println("Hello World");
-		
-		System.out.println("Tryiing to create a merge conflict...");
-		
 		System.out.println("That took way too much effort...");
 		System.out.println("See ya later!");
 	}
