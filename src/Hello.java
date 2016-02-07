22b1c20db5dcfb023551e63d393c90d293ce5c4b
Nick Hamel <nicholas.g.hamel@maine.edu>
2016-02-07 18:36:31 -0500

    Once again trying to create a merge conflict...

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Here ye Here ye: important message");
+		
+		System.out.println("Creating a merge conflict here...");
+		
 		System.out.println("Hello World");
 		System.out.println("That took way too much effort...");
 		System.out.println("See ya later!");
